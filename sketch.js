811cb5fce80f6e13a1179d9f8de264fa875646c8
LegoMaxCod3r <93746195+LegoMaxCod3r@users.noreply.github.com>
2021-11-24 18:51:27 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let ball;
+var vx=2;
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  ball=createSprite(100,100,100,100);
+}
+
+function draw() {
+  background("lightblue");
+
+  console.log(ball.position.x);
+
+  if(ball.position.x<=50 || ball.position.x>=width-50){
+
+    vx = -vx;
+
+
+  }
+  ball.velocity.x=vx;
+
+
+  drawSprites();
+}
+
+
